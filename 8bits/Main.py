6d2dd19f2a4b28ef8095e52a801ff5ad5160ab8e
LiXuhui <lixuhui@eaglechip.com>
2022-08-16 14:07:47 +0800

    Change  Main.py summerClassNetwork.py

--- 8bits/Main.py
+++ 8bits/Main.py
@@ -3,10 +3,10 @@
 from summerNetwork_h import *
 def main():
     V = [1, 2, 3, 4, 5, 6]
-    E = [Edge(1, 2, 2), Edge(1, 3, 3), 
-        Edge(2, 4, 3), Edge(2, 5, 1),
-        Edge(3, 4, 1), Edge(3, 5, 1), 
-        Edge(4, 6, 2), Edge(5, 6, 3)]
+    E = [Edge(1, 2, 2, 1), Edge(1, 3, 3, 1), 
+        Edge(2, 4, 3, 1), Edge(2, 5, 1, 1),
+        Edge(3, 4, 1, 1), Edge(3, 5, 1, 1), 
+        Edge(4, 6, 2, 1), Edge(5, 6, 3, 1)]
     s, t = 1, 6
     G = Network(V, E, s, t)
     G.display()
@@ -16,6 +16,7 @@ def main():
     ford_fulkerson.display()
     G = ford_fulkerson.return_network()
     G.display()
-   
+
+
 if __name__ == "__main__":
     main()
--- 8bits/summerClassNetwork.py
+++ 8bits/summerClassNetwork.py
@@ -16,6 +16,9 @@ class Network():
 
     def get_from_edges(self, v):
         '''
+        获得从 v 节点 流出的边
+        '''
+        '''
         get the from_edge of a node
         '''
         return [edge for edge in self.E if edge.is_from(v)]
@@ -32,18 +35,23 @@ class Network():
 
 
     def flows_from(self, v):
+        ''' 获得deg+(v) '''
         ''' deg+(v) '''
         edges = self.get_from_edges(v)
         return sum([e.flow for e in edges])
 
     def flows_to(self, v):
         ''' deg-(v) '''
-        edges = self.get_from_edges(v)
+        edges = self.get_to_edges(v)
         return sum([e.flow for e in edges])
 
     def self_check(self, s, t):
         ''' 网络中s的流出是否等于t的流入
         :return s流出=t流入 返回true
+        注意，自检只检查了s的流出和t的流入，没有检查s和t的流入流出，
+        没有检查其他节点的流入流出是否守恒
+        没有检查每条边的cap是否为正
+        没有检查每条边的flow是否为正，是否过载
         '''
         return int(self.flows_to(s)) == int(self.flows_from(t))
 
@@ -51,8 +59,8 @@ class Network():
         if not self.self_check(self.s,self.t):
             print("error! This network doesnot obdy thelaw of flow_conservation")
             return
-        print(' %-10s%-10s%-8s' % ('edge', 'cap', 'flow'))
+        print('%-10s%-10s%-8s' % ('edge', 'cap', 'flow'))
         for e in self.E:
-            print(' %-10s%-10s%-8s' % 
+            print('%-10s%-10s%-8s' % 
                     (e, e.cap, e.flow if e.flow < e.cap else str(e.flow)+"*"))
 
